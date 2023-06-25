dfce001f096ec0260c47783a089fa8d525a11f93
Cameron Johnson <cameronrossjohnson@gmail.com>
2023-06-24 19:50:32 -0500

    Auth0 Templates

---
+++ auth0/Auth0Provider.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
+import App from './App';
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <Auth0Provider
+    domain={process.env.REACT_APP_AUTH0_DOMAIN || ''}
+    clientId={process.env.REACT_APP_AUTH0_CLIENT_ID || ''}
+    authorizationParams={{
+      redirect_uri: process.env.REACT_APP_ORIGIN_URL
+    }}
+  >
+    <App />
+  </Auth0Provider>,
+);
