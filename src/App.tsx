fcb5bc383a7727c0d8d9da3c6f4c43f56888c3cc
Cameron Johnson <cameronrossjohnson@gmail.com>
2024-12-22 17:38:47 -0600

    Add appbar component

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import {CJDialog} from "./screens/CJDialog";
 import {ImportText} from "./utilities/ImportText";
+import {AppBar} from "./navigation/AppBar";
 
 function App() {
   return (
       <div className="App">
+          <AppBar title={'Frontend Templates'} routes={['Bookmarks', 'Books']} onChange={(route) => alert(route)} />
           <h1>Frontend Templates</h1>
           <h2>CJDialog</h2>
           <CJDialog title={'My Modal'} buttonTitle={'Open Dialog'}>
